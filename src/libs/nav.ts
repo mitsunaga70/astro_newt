379040d470c81699bee8f53fbbb7c6d9e9b379f6
mitsunaga <mitsunaga@fieldweb.co.jp>
2023-10-05 10:17:29 +0900

    イベント調整

--- src/libs/nav.ts
+++ src/libs/nav.ts
@@ -1,22 +1,22 @@
 export const navItems = [
   {
     text: "TOP",
-
     link: "/",
+    load: false,
   },
   {
     text: "BLOG",
-
     link: "/articles/",
+    load: false,
   },
   {
     text: "NEWS",
-
     link: "/news/",
+    load: false,
   },
   {
     text: "EVENT",
-
     link: "/events/",
+    load: true,
   },
 ];
